e54465c06488bf9563c83343d5f2b94388d655a2
Max Nachlinger <max.nachlinger@gmail.com>
2019-01-12 09:12:34 -0800

    [PATCH] Updated dependencies

--- tests/test-cache.ts
+++ tests/test-cache.ts
@@ -1,7 +1,7 @@
 import {ICacheClient} from '../index';
 
 export default (initialValue: any = {}): ICacheClient => {
-	const cache: { [s: string]: any } = initialValue;
+	const cache = initialValue;
 	return {
 		set: (key, value) => (cache[key] = value),
 		get: async key => Promise.resolve(cache[key])
